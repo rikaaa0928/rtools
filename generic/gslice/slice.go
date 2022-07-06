bffdcbfb9bc903994aec385b5377c4b6f2e7aa0e
rikaaa0928 <w1zirn1996@gmail.com>
2022-07-06 16:12:29 +0800

    slice sort & compare

--- generic/gslice/slice.go
+++ generic/gslice/slice.go
@@ -1,10 +1,40 @@
 package gslice
 
-func InSlice[T comparable](set []T, obj T) bool {
-	for _, v := range set {
+import (
+	"sort"
+
+	"golang.org/x/exp/constraints"
+)
+
+func InSlice[T comparable](slice []T, obj T) bool {
+	for _, v := range slice {
 		if v == obj {
 			return true
 		}
 	}
 	return false
+}
+
+func Sort[T constraints.Ordered](slice []T) {
+	sort.Slice(slice, func(i, j int) bool {
+		return slice[i] < slice[j]
+	})
+}
+
+func CompareOrdered[T constraints.Ordered](slice, slice2 []T) bool {
+	Sort(slice)
+	Sort(slice2)
+	return Compare(slice, slice2)
+}
+
+func Compare[T comparable](slice, slice2 []T) bool {
+	if len(slice) != len(slice2) {
+		return false
+	}
+	for i := range slice {
+		if slice[i] != slice2[i] {
+			return false
+		}
+	}
+	return true
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/rikaaa0928/rtools
 
 go 1.18
 
+require golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
