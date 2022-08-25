caa243a4934e316075f5c49fd60378ce5e2999d2
rikaaa0928 <wangzhidong1@xiaomi.com>
2022-08-25 13:53:26 +0800

    intordefault float support

--- rstring/int.go
+++ rstring/int.go
@@ -5,6 +5,10 @@ import "strconv"
 func IntOrDefault(i string, d int64) int64 {
 	res, err := strconv.ParseInt(i, 10, 64)
 	if err != nil {
+		f, err := strconv.ParseFloat(i, 64)
+		if err == nil {
+			return int64(f)
+		}
 		return d
 	}
 	return res
