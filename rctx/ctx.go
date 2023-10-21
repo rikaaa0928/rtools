7c1771702f329c7a6b67240e75a086f84e75d567
rikaaa0928 <wangzhidong1@xiaomi.com>
2023-10-21 12:16:04 +0800

    ctx map refactor

--- rctx/ctx.go
+++ rctx/ctx.go
@@ -6,6 +6,82 @@ import (
 
 var defaultMKey interface{}
 
+type mapWithIKey[K comparable, V any] struct {
+	iKey interface{}
+}
+
+func (mk *mapWithIKey[K, V]) WithMapValue(ctx context.Context, k K, v V) context.Context {
+	m, ok := ctx.Value(mk.iKey).(map[K]interface{})
+	if !ok {
+		m = make(map[K]interface{}, 1)
+		m[k] = v
+		return context.WithValue(ctx, mk.iKey, m)
+	}
+	m[k] = v
+	return ctx
+}
+
+func (mk *mapWithIKey[K, V]) MapValueOK(ctx context.Context, k K) (*V, bool) {
+	val := ctx.Value(mk.iKey)
+	m, ok := val.(map[K]interface{})
+	if !ok {
+		return nil, false
+	}
+	v, ok := m[k]
+	if !ok {
+		return nil, false
+	}
+	tv, ok := v.(V)
+	if !ok {
+		return nil, false
+	}
+	return &tv, true
+}
+
+func (mk *mapWithIKey[K, V]) MapValue(ctx context.Context, k K) *V {
+	v, ok := mk.MapValueOK(ctx, k)
+	if !ok {
+		return nil
+	}
+	return v
+}
+
+func (mk *mapWithIKey[K, V]) Map(ctx context.Context) map[K]interface{} {
+	m, ok := ctx.Value(mk.iKey).(map[K]interface{})
+	if !ok {
+		return nil
+	}
+	return m
+}
+
+func (mk *mapWithIKey[K, V]) TypedMap(ctx context.Context) map[K]V {
+	m, ok := ctx.Value(mk.iKey).(map[K]interface{})
+	if !ok {
+		return nil
+	}
+	nm := make(map[K]V, len(m))
+	for k, v := range m {
+		tv, ok := v.(V)
+		if !ok {
+			continue
+		}
+		nm[k] = tv
+	}
+	return nm
+}
+
+type MapWithIndexKey[K comparable, V any] interface {
+	WithMapValue(ctx context.Context, k K, v V) context.Context
+	MapValueOK(ctx context.Context, k K) (*V, bool)
+	MapValue(ctx context.Context, k K) *V
+	Map(ctx context.Context) map[K]interface{}
+	TypedMap(ctx context.Context) map[K]V
+}
+
+func MapKey[K comparable, V any](mk interface{}) MapWithIndexKey[K, V] {
+	return &mapWithIKey[K, V]{mk}
+}
+
 func DefaultMapKey(k interface{}) {
 	defaultMKey = k
 }
@@ -35,81 +111,42 @@ func Pointer[V any, K any](ctx context.Context, k K) *V {
 	return v
 }
 
-func DWithMapValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
-	return WithMapValue[any, K, V](ctx, defaultMKey, k, v)
+//func DWithMapValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
+//	return WithMapValue[any, K, V](ctx, defaultMKey, k, v)
+//}
+
+func WithMapValue[K comparable, V any](ctx context.Context, k K, v V) context.Context {
+	return MapKey[K, V](defaultMKey).WithMapValue(ctx, k, v)
 }
 
-func WithMapValue[M any, K comparable, V any](ctx context.Context, mk M, k K, v V) context.Context {
-	m, ok := ctx.Value(mk).(map[K]interface{})
-	if !ok {
-		m = make(map[K]interface{}, 1)
-		m[k] = v
-		return context.WithValue(ctx, mk, m)
-	}
-	m[k] = v
-	return ctx
+//func DMapValueOK[V any, K comparable](ctx context.Context, k K) (*V, bool) {
+//	return MapValueOK[V, any, K](ctx, defaultMKey, k)
+//}
+
+func MapValueOK[V any, K comparable](ctx context.Context, k K) (*V, bool) {
+	return MapKey[K, V](defaultMKey).MapValueOK(ctx, k)
 }
 
-func DMapValueOK[V any, K comparable](ctx context.Context, k K) (*V, bool) {
-	return MapValueOK[V, any, K](ctx, defaultMKey, k)
+//func DMapValue[V any, K comparable](ctx context.Context, k K) *V {
+//	return MapValue[V, any, K](ctx, defaultMKey, k)
+//}
+
+func MapValue[V any, K comparable](ctx context.Context, k K) *V {
+	return MapKey[K, V](defaultMKey).MapValue(ctx, k)
 }
 
-func MapValueOK[V any, M any, K comparable](ctx context.Context, mk M, k K) (*V, bool) {
-	m, ok := ctx.Value(mk).(map[K]interface{})
-	if !ok {
-		return nil, false
-	}
-	v, ok := m[k]
-	if !ok {
-		return nil, false
-	}
-	tv, ok := v.(V)
-	if !ok {
-		return nil, false
-	}
-	return &tv, true
+//func DMap[K comparable](ctx context.Context) map[K]interface{} {
+//	return Map[K, any](ctx, defaultMKey)
+//}
+
+func Map[K comparable](ctx context.Context) map[K]interface{} {
+	return MapKey[K, interface{}](defaultMKey).Map(ctx)
 }
 
-func DMapValue[V any, K comparable](ctx context.Context, k K) *V {
-	return MapValue[V, any, K](ctx, defaultMKey, k)
-}
+//func DVMap[K comparable, V any](ctx context.Context) map[K]V {
+//	return VMap[K, V, any](ctx, defaultMKey)
+//}
 
-func MapValue[V any, M any, K comparable](ctx context.Context, mk M, k K) *V {
-	v, ok := MapValueOK[V](ctx, mk, k)
-	if !ok {
-		return nil
-	}
-	return v
-}
-
-func DMap[K comparable](ctx context.Context) map[K]interface{} {
-	return Map[K, any](ctx, defaultMKey)
-}
-
-func Map[K comparable, M any](ctx context.Context, mk M) map[K]interface{} {
-	m, ok := ctx.Value(mk).(map[K]interface{})
-	if !ok {
-		return nil
-	}
-	return m
-}
-
-func DVMap[K comparable, V any](ctx context.Context) map[K]V {
-	return VMap[K, V, any](ctx, defaultMKey)
-}
-
-func VMap[K comparable, V any, M any](ctx context.Context, mk M) map[K]V {
-	m, ok := ctx.Value(mk).(map[K]interface{})
-	if !ok {
-		return nil
-	}
-	nm := make(map[K]V, len(m))
-	for k, v := range m {
-		tv, ok := v.(V)
-		if !ok {
-			continue
-		}
-		nm[k] = tv
-	}
-	return nm
+func TypedMap[K comparable, V any](ctx context.Context) map[K]V {
+	return MapKey[K, V](defaultMKey).TypedMap(ctx)
 }
--- rctx/ctx_test.go
+++ rctx/ctx_test.go
@@ -12,42 +12,17 @@ func TestPointer(t *testing.T) {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "k1", "v1")
 	ctx = rctx.WithPointer(ctx, "k2", "vv1")
-	ctx = rctx.WithMapValue(ctx, "k3", "k4", "vvv1")
 	t.Log(ctx.Value("k1"))
 	t.Log(*rctx.Pointer[string](ctx, "k2"))
 	t.Log(rctx.Pointer[string](ctx, "kk2"))
-	t.Log(*rctx.MapValue[string](ctx, "k3", "k4"))
 	testPointer(ctx, t)
 	t.Log(ctx.Value("k1"))
 	t.Log(*ctx.Value("k2").(*string))
-	t.Log(*rctx.MapValue[string](ctx, "k3", "k4"))
-	t.Log(*rctx.MapValue[string](ctx, "k3", "k5"))
-	t.Log(*rctx.MapValue[int](ctx, "k3", "k6"))
-	bytes, err := json.Marshal(rctx.Map[string](ctx, "k3"))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Log(string(bytes))
-	bytes, err = json.Marshal(rctx.VMap[string, string](ctx, "k3"))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Log(string(bytes))
 }
 
 func testPointer(ctx context.Context, t *testing.T) {
 	ctx = context.WithValue(ctx, "k1", "v2")
-	ctx = rctx.WithPointer(ctx, "k2", "vv2")
-	ctx = rctx.WithMapValue(ctx, "k3", "k4", "vvv1")
-	ctx = rctx.WithMapValue(ctx, "k3", "k5", "vvvx")
-	ctx = rctx.WithMapValue(ctx, "k3", "k6", 6)
 	t.Log(ctx.Value("k1"))
-	t.Log(*rctx.Pointer[string](ctx, "k2"))
-	t.Log(*rctx.MapValue[string](ctx, "k3", "k4"))
-	t.Log(*rctx.MapValue[string](ctx, "k3", "k5"))
-	t.Log(*rctx.MapValue[int](ctx, "k3", "k6"))
 }
 
 type interX interface {
@@ -66,23 +41,23 @@ func TestInterface(t *testing.T) {
 	ctx := context.Background()
 	var x *structX = &structX{X: 1}
 	var y interX = &structX{X: 2}
-	ctx = rctx.WithMapValue(ctx, "k1", "k2", x)
-	ctx = rctx.WithMapValue(ctx, "k3", "k4", y)
-	bytes, err := json.Marshal(rctx.Map[string](ctx, "k1"))
+	ctx = rctx.MapKey[string, *structX]("k1").WithMapValue(ctx, "k2", x)
+	ctx = rctx.MapKey[string, interX]("k3").WithMapValue(ctx, "k4", y)
+	bytes, err := json.Marshal(rctx.MapKey[string, interface{}]("k1").Map(ctx))
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	t.Log(string(bytes))
-	bytes, err = json.Marshal(rctx.Map[string](ctx, "k3"))
+	bytes, err = json.Marshal(rctx.MapKey[string, interface{}]("k3").Map(ctx))
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	t.Log(string(bytes))
-	xxv := rctx.MapValue[*structX](ctx, "k1", "k2")
+	xxv := rctx.MapKey[string, *structX]("k1").MapValue(ctx, "k2")
 	t.Logf("%v,%v,%v", xxv, *xxv, **xxv)
-	xxy := *rctx.MapValue[interX](ctx, "k3", "k4")
+	xxy := *rctx.MapKey[string, interX]("k3").MapValue(ctx, "k4")
 	t.Logf("%v", xxy.A())
 }
 
@@ -127,15 +102,15 @@ func TestMapValue(t *testing.T) {
 	type a string
 	ctx := context.Background()
 	var kk a = "k"
-	ctx = rctx.WithMapValue(ctx, kk, "k1", "v2")
-	t.Log(rctx.MapValue[string](ctx, "k", "k1"))
-	t.Log(*rctx.MapValue[string](ctx, a("k"), "k1"))
-	t.Log(*rctx.MapValue[string](ctx, kk, "k1"))
+	ctx = rctx.MapKey[string, string](kk).WithMapValue(ctx, "k1", "v2")
+	t.Log(rctx.MapKey[string, string]("k").MapValue(ctx, "k1"))
+	t.Log(*rctx.MapKey[string, interface{}](a("k")).MapValue(ctx, "k1"))
+	t.Log(*rctx.MapKey[string, string](kk).MapValue(ctx, "k1"))
 	var k2 interface{} = kk
-	t.Log(*rctx.MapValue[string](ctx, k2, "k1"))
+	t.Log(*rctx.MapKey[string, string](k2).MapValue(ctx, "k1"))
 	rctx.DefaultMapKey(kk)
-	t.Log(*rctx.DMapValue[string](ctx, "k1"))
-	ctx = rctx.DWithMapValue(ctx, "k2", "vvv")
-	t.Log(rctx.DVMap[string, string](ctx))
-	t.Log(rctx.DMap[string](ctx))
+	t.Log(*rctx.MapValue[string, string](ctx, "k1"))
+	ctx = rctx.WithMapValue(ctx, "k2", "vvv")
+	t.Log(rctx.TypedMap[string, string](ctx))
+	t.Log(rctx.Map[string](ctx))
 }
